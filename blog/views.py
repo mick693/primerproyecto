14805287d90cb61a558cf3282ff2c8b68d06b5bc
miguel <jmiguelcl47@gmail.com>
2017-08-30 19:11:33 -0600

    css

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def listar_publicacion(request):
-    return render(request,'blog/listar_publicacion.html',{})
+    publi=Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request,'blog/listar_publicacion.html',{'publi':publi})
 # Create your views here.
