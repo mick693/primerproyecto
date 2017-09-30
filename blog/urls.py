0e4f4e15e2e4da93e3eee89f285cba5fbe9ac121
miguel <jmiguelcl47@gmail.com>
2017-09-29 19:00:23 -0600

    publicar listar eliminar editar

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns =[
     url(r'^$',views.listar_publicacion),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail,name='post_detail'),
+    url(r'^post/nueva/$', views.nueva_publicacion,name='nueva_publicacion'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 from django.shortcuts import render, get_object_or_404
+
 
 def listar_publicacion(request):
     publi=Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
@@ -9,4 +11,47 @@ def listar_publicacion(request):
 # Create your views here.
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})     
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def nueva_publicacion(request):
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/nueva_publicacion.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/nueva_publicacion.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.fecha_publicacion=timezone.now()
+    post.save()
+    return render(request, 'blog/post_draft_list.html', {'post': post})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    publi=Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request,'blog/listar_publicacion.html',{'publi':publi})
